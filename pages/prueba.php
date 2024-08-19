ea91b611f9e5db474cd4408666d446811af3e7b6
Alejandro Hernandez <alejandrohernandezdeveloper@gmail.com>
2024-08-19 10:57:44 -0500

    Se deja listo el dashboard, a falta de enlaces

--- pages/prueba.php
+++ pages/prueba.php
@@ -1,11 +1,5 @@
-<?php
-include '../config/config.php';
-include '../config/check_login.php';
-
-?>
-
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
